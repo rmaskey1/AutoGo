3cd7bac7cae4e9d9e403d78b32d1121247ee7e56
Ayush Shresth <127001000+ayushshresth021@users.noreply.github.com>
2023-05-08 13:15:51 -0700

    created login and signup page

--- create.php
+++ create.php
@@ -1,17 +1,17 @@
 <?php
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["firstname"]) && isset($_POST["lastname"]) && isset($_POST["phone"]) && isset($_POST["email"]) && isset($_POST["address"]))
+    if (isset($_POST["userid"]) && isset($_POST["pass"]) && isset($_POST["name"]) && isset($_POST["name-2"]) && isset($_POST["phone"]) && isset($_POST["email"]) && isset($_POST["address"]))
     {
-      $username = $_POST["username"];
-      $password = $_POST["password"];
-      $firstname = $_POST["firstname"];
-      $lastname = $_POST["lastname"];
+      $username = $_POST["userid"];
+      $password = $_POST["pass"];
+      $firstname = $_POST["name"];
+      $lastname = $_POST["name-2"];
       $phone = $_POST["phone"];
       $email = $_POST["email"];
       $address = $_POST["address"];
 
       // Create connection
-      $conn = mysqli_connect("localhost", "root", "admin", "autogo");
+      $conn = mysqli_connect("localhost", "root", "", "autogo");
 
       if (!$conn) {
           die("Connection failed: " . mysqli_connect_error());
@@ -35,87 +35,88 @@
   }
 ?>
 
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="styles2.css">
 
 
-    <div class="div-block"><img src="images/autogo_logo.png" class="center" loading="lazy" sizes="200px" srcset="images/autogo_logo-p-500.png 500w, images/autogo_logo.png 728w" alt=""></div>
-  </div>
-  <div class = "topBox">
-    <div class = "leftBoxL">
-      <button class="toplink"><a href="index.php" id="topcolor">Autogo</a></button>
-    </div>
-
-    <div class = "buttonGroup">
-      <div class = "rightBoxR">
-        <button class="toplink"><a href="index.php" id="topcolor">Logout</a></button>
-      </div>
-    </div>
-</div>
-
-    <title>Autogo Sign up</title>
-  </head>
-  <body>
-
-    <div class = "bottomBox">
-      <div class ="useraccount">
-        <div class = "Box">
-        CREATE AN ACCOUNT
+<!DOCTYPE html><!--  This site was created in Webflow. https://www.webflow.com  -->
+<!--  Last Published: Sat May 06 2023 03:14:17 GMT+0000 (Coordinated Universal Time)  -->
+<html data-wf-page="6451ad8f588f28ce3b7a46b5" data-wf-site="644ae8923d92094098ec3617">
+<head>
+  <meta charset="utf-8">
+  <title>Signup</title>
+  <meta content="Signup" property="og:title">
+  <meta content="Signup" property="twitter:title">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta content="Webflow" name="generator">
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="css/autogo.webflow.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+  <script type="text/javascript">WebFont.load({  google: {    families: ["Changa One:400,400italic","Droid Serif:400,400italic,700,700italic","Bitter:400,700,400italic"]  }});</script>
+  <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+  <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+  <link href="images/webclip.png" rel="apple-touch-icon">
+</head>
+<body>
+  <div class="navbar-logo-left-2 wf-section">
+    <div data-animation="default" data-collapse="medium" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="navbar-logo-left-container-2 shadow-three w-nav">
+      <div class="container-3">
+        <div class="navbar-wrapper-3">
+          <a href="#" class="navbar-brand-3 w-nav-brand"><img src="images/autogo_logo-p-500.png" loading="lazy" alt="" class="image-5"></a>
+          <nav role="navigation" class="nav-menu-wrapper-3 w-nav-menu">
+            <ul role="list" class="nav-menu-two-2 w-list-unstyled">
+              <li>
+                <a href="homepage.php" class="nav-link-3">Home</a>
+              </li>
+              <li>
+                <a href="#" class="nav-link-3">About</a>
+              </li>
+              <li class="mobile-margin-top-12">
+                <a href="login.php" class="button-2 w-button">Login</a>
+              </li>
+            </ul>
+          </nav>
+          <div class="menu-button-3 w-nav-button">
+            <div class="w-icon-nav-menu"></div>
+          </div>
         </div>
-        <form action="#" method="post">
-            <div class = "box">
-            <label>Email: </label>
-            <input type="email" name="email"
-                    autocomplete="off"
-                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                    required />
-            </div>
-
-            <div class = "box">
-                <label>User ID: </label>
-                <input type="text" name="username" />
-            </div>
-
-            <div class = "box">
-                <label>Password: </label>
-                <input type="password" name="password" autocomplete="off"
-                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters">
-                <required/>
-            </div>
-            <div class = "box">
-                <label>First Name: </label>
-                <input type="firstname" name="firstname" autocomplete="off"
-                        onkeypress="return (event.charCode >= 97 && event.charCode <= 122) || (event.charCode >= 65 && event.charCode <= 90)";
-                        required/>
-            </div>
-            <div class = "box">
-                <label>Last Name: </label>
-                <input type="lastname" name="lastname" autocomplete="off"
-                        onkeypress="return (event.charCode >= 97 && event.charCode <= 122) || (event.charCode >= 65 && event.charCode <= 90)";
-                        required/>
-            </div>
-            <div class = "box">
-                <label>Phone: </label>
-                <input type="phone" name="phone" autocomplete="off"
-                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                        placeholder="123-456-7890"
-                        required/>
-            </div>
-            <div class = "box">
-                <label>Address: </label>
-                <input type="address" name="address" autocomplete="off"
-                        minlength ="1"
-                        maxlength="50"
-                        required/>
-            </div><br>
-            <div class = "boxcenter">
-              <input type="submit" value="Submit"> <!-- on submit save the entered fields into variables and then send the user to personal information collection page-->
-            </div>
-        </form>
       </div>
     </div>
-  </body>
+  </div>
+  <div class="columns-2 w-row">
+    <div class="w-col w-col-6">
+      <div class="container-2">
+        <div class="section-title">
+          <h2 class="text">Create Account</h2>
+          <div class="text-2">Welcome to autoGo!</div>
+        </div>
+        <div class="form-wrapper w-form">
+        <form id="webflow-form" name="webflow-form" data-name="Webflow Form" method="post" action="create.php" class="form-3">
+          <div class="input-wrapper"><label for="email" class="form-block-label">Email</label><input type="Email" class="form-text-input w-input" maxlength="256" name="email" data-name="Name 2" placeholder="Enter your email" id="email"></div>
+          <div class="input-wrapper"><label for="userid" class="form-block-label">UserId</label><input type="text" class="form-text-input w-input" maxlength="256" name="userid" data-name="Name 2" placeholder="Enter a user name" id="userid"></div>
+          <div class="input-wrapper"><label for="pass" class="form-block-label">Password</label><input type="Password" class="form-text-input-2 w-input" maxlength="256" name="pass" data-name="Name" placeholder="Password" id="pass"pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"><required/></div>
+          <div class="input-wrapper"><label for="name" class="form-block-label">First Name</label><input type="text" class="form-text-input w-input" maxlength="256" name="name" data-name="Name 2" placeholder="First Name" id="name"></div>
+          <div class="input-wrapper"><label for="name-2" class="form-block-label">Last Name</label><input type="text" class="form-text-input w-input" maxlength="256" name="name-2" data-name="Name" placeholder="Last Name" id="name-2"></div>
+          <div class="input-wrapper"><label for="phone" class="form-block-label">Phone Number</label><input type="phone" class="form-text-input w-input" maxlength="256" name="phone" data-name="Name 2"  id="phone"pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="123-456-7890"></div>
+          <div class="input-wrapper"><label for="address" class="form-block-label">Address</label><input type="Address" class="form-text-input w-input" maxlength="256" name="address" data-name="Name 2" placeholder="Address" id="address"></div>
+            <div class="input-wrapper"></div><input type="submit" value="Submit" data-wait="Waiting..." class="form-button w-button">
+            <a href="login.php" class="button-4 w-button">Already a customer? Click here to Login!</a>
+          </form>
+          <div class="w-form-done">
+            <div>Thank you! Your submission has been received!</div>
+          </div>
+          <div class="w-form-fail">
+            <div>Oops! Something went wrong while submitting the form.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="w-col w-col-6"><img src="images/photo-1564188537512-f6bd010d1e2a.jpeg" loading="lazy" sizes="100vw" srcset="images/photo-1564188537512-f6bd010d1e2a-p-500.jpeg 500w, images/photo-1564188537512-f6bd010d1e2a-p-800.jpeg 800w, images/photo-1564188537512-f6bd010d1e2a.jpeg 1000w" alt=""></div>
+  </div>
+  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=644ae8923d92094098ec3617" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="js/webflow.js" type="text/javascript"></script>
+  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+</body>
 </html>
--- login.php
+++ login.php
@@ -1,29 +1,26 @@
 <?php
   session_start();
 
-
   error_reporting(E_ERROR | E_PARSE);
+
   $username = "$_SESSION[username]";
   if ("$_SESSION[username]" === "incorrect password" || "$_SESSION[username]" === "One of the information is empty" || "$_SESSION[username]" === "Account has been created! Please log in with your brand new account."){
     echo $username;
   }
 
   if (isset($_POST["username"]) &&
-      isset($_POST['password'])) {
+      isset($_POST["password"])) {
     if ($_POST["username"] && $_POST["password"]) {
       $username = $_POST["username"];
       $password = $_POST["password"];
       $_SESSION['username'] = $username;
 
       //create connection
-      $conn = mysqli_connect("localhost", "root", "admin", "autogo");
+      $conn = mysqli_connect("localhost", "root", "", "autogo");
 
       //Check connection
       if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
-      }
-      else {
-        echo "connected";
       }
 
       // select user, check for password
@@ -62,54 +59,83 @@
 
   ?>
 
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="styles2.css">
-    <title>Autogo Account Login</title>
-  </head>
-  <body>
-<!-- Top of bar box. Designed with CSS flexdispalays.  -->
-      <div class = "topBox">
-        <div class = "leftBoxL">
-          <button class="toplink"><a href="homepage.php" id="topcolor">Autogo</a></button>
-        </div>
-        <div class = "buttonGroup">
 
-          <div class = "rightBoxL">
-            <button class="toplink"><a href="aboutUsPage.html" id="topcolor">Home</a></button>
-          </div>
-          <div class = "rightBoxR">
-            <button class="toplink"><a href="login.php" id="topcolor">Log Out</a></button>
-          </div>
 
+<!DOCTYPE html><!--  This site was created in Webflow. https://www.webflow.com  -->
+<!--  Last Published: Sat May 06 2023 03:14:17 GMT+0000 (Coordinated Universal Time)  -->
+<html data-wf-page="6451a8c92eebfa5839a89f9b" data-wf-site="644ae8923d92094098ec3617">
+<head>
+  <meta charset="utf-8">
+  <title>Login</title>
+  <meta content="Login" property="og:title">
+  <meta content="Login" property="twitter:title">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta content="Webflow" name="generator">
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="css/autogo.webflow.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+  <script type="text/javascript">WebFont.load({  google: {    families: ["Changa One:400,400italic","Droid Serif:400,400italic,700,700italic","Bitter:400,700,400italic"]  }});</script>
+  <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+  <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+  <link href="images/webclip.png" rel="apple-touch-icon">
+</head>
+<body>
+  <div class="navbar-logo-left-2 wf-section">
+    <div data-animation="default" data-collapse="medium" data-duration="400" data-easing="ease" data-easing2="ease" role="banner" class="navbar-logo-left-container-2 shadow-three w-nav">
+      <div class="container-3">
+        <div class="navbar-wrapper-3">
+          <a href="#" class="navbar-brand-3 w-nav-brand"><img src="images/autogo_logo-p-500.png" loading="lazy" alt="" class="image-5"></a>
+          <nav role="navigation" class="nav-menu-wrapper-3 w-nav-menu">
+            <ul role="list" class="nav-menu-two-2 w-list-unstyled">
+              <li>
+                <a href="index.php" class="nav-link-3">Home</a>
+              </li>
+              <li>
+                <a href="about.php" class="nav-link-3">About</a>
+              </li>
+              <li class="mobile-margin-top-12">
+                <a href="login.php" class="button-2 w-button">Login</a>
+              </li>
+            </ul>
+          </nav>
+          <div class="menu-button-3 w-nav-button">
+            <div class="w-icon-nav-menu"></div>
+          </div>
         </div>
       </div>
-      <div class = "bottomBox">
-        <div class = "centerBox">
-            <div class = "centerLeftBox">
-                <video width="500rem" height="280rem" controls>
-                    <source src="vid.mp4" type="video/mp4">
-                    Your browser does not support the video tag.
-                </video>
-            </div>
+    </div>
+  </div>
+  <div class="w-row">
+    <div class="w-col w-col-6"><img src="images/3jjqle26oyqlppuy.jpg" loading="lazy" sizes="(max-width: 479px) 96vw, (max-width: 767px) 97vw, (max-width: 991px) 48vw, 49vw" srcset="images/3jjqle26oyqlppuy-p-500.jpg 500w, images/3jjqle26oyqlppuy-p-800.jpg 800w, images/3jjqle26oyqlppuy-p-1080.jpg 1080w, images/3jjqle26oyqlppuy.jpg 1280w" alt="" class="image-4"></div>
+    <div class="column w-col w-col-6">
+      <div class="container-2">
+        <div class="section-title">
+          <h2 class="text">LOGIN</h2>
+          <div class="text-2">Welcome Back!</div>
         </div>
-        <div class = "centerBox2">
-            <div class = "centerRightBox">
-              <form action="login.php" method="post">
-                <h2 class="custom">Welcome Back!</h2>
-                  <label for="username">Username:</label><br>
-                    <input type="text" name="username"><BR><br>
-                      <label for="username">Password:</label><br>
-                      <input type="password" name="password"><BR><BR>
-                      <input type="submit">
-                      <br><br>
-                      <button class="otherOptionButton"><a href="createUser.php" id="topcolor">Don't have an account? Click here!</a></button>
-                      <br>
-                      <a class href="admin.php" style="text-decoration:none;">‎ ‎ </a>
-              </form>
-            </div>
+        <div class="form-wrapper w-form">
+          <form id="webflow-form" name="webflow-form" data-name="Webflow Form" method="post" class="form-3" action="login.php">
+            <div class="input-wrapper"><label for="username" class="form-block-label">User Name</label><input type="text" class="form-text-input w-input" maxlength="256" name="username" data-name="Name" placeholder="Your user name" id="username"></div>
+            <div class="input-wrapper"><label for="password" class="form-block-label">Password</label><input type="password" class="form-text-input-2 w-input" maxlength="256" name="password" data-name="Name" placeholder="Password" id="password"></div>
+            <div class="input-wrapper"></div><input type="submit" value="Submit" data-wait="Logging in..." class="form-button w-button">
+            <a href="create.php" class="button-4 w-button">Don&#x27;t have an account? Click here to create an account!</a>
+          </form>
+          <div class="w-form-done">
+            <div>Thank you! Your submission has been received!</div>
           </div>
+          <div class="w-form-fail">
+            <div>Oops! Something went wrong while submitting the form.</div>
+          </div>
+        </div>
       </div>
-  </body>
+    </div>
+  </div>
+  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=644ae8923d92094098ec3617" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="js/webflow.js" type="text/javascript"></script>
+  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+</body>
 </html>
